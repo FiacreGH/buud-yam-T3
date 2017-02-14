4f304cad4733c0593ae3172af4767ea3d6c61f68
Fabien Udriot <fabien.udriot@ecodev.ch>
2017-02-14 11:26:33 +0100

    [CONFIG] More config for EXT:fluidcontent_core

--- htdocs/typo3conf/AdditionalConfiguration.php
+++ htdocs/typo3conf/AdditionalConfiguration.php
@@ -6,3 +6,7 @@ require_once (PATH_site . 'typo3conf/ext/speciality_ecodev/Configuration/PHP/Typ
 # Frontend Settings
 $GLOBALS['TYPO3_CONF_VARS']['FE']['pageNotFound_handling'] = '/404/';
 $GLOBALS['TYPO3_CONF_VARS']['FE']['pageUnavailable_handling'] = '/503/';
+
+// Declare Content Rendering for the Frontend
+$GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'] = array('fluidcontentcore/Configuration/TypoScript/');
+$GLOBALS['TYPO3_CONF_VARS']['FE']['activateContentAdapter'] = 0;
